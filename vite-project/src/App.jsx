6b35eee20eb4d83e6702a43efcdd92f226377469
Heshani833 <heshanishehana123@gmail.com>
2025-09-22 18:57:54 +0530

    created navbar

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,12 +1,17 @@
-import React from 'react'
-import "./App.css"
+import React from "react";
+import "./App.css";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
-    <div>
-      App
-    </div>
-  )
-}
+    <>
+      <Navbar />
+      <div
+        className="main flex items-center justify-between bg-red-500"
+        style={{ height: "calc(100vh - 90px)" }}
+      ></div>
+    </>
+  );
+};
 
-export default App
+export default App;
---
+++ vite-project/src/components/Navbar.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { BrainCircuit } from "lucide-react";
+import { Sun } from "lucide-react";
+import Editor from "@monaco-editor/react";
+
+const Navbar = () => {
+  return (
+    <>
+      <div
+        className="nav flex items-center justify-between  h-[90px] bg-zinc-800"
+        style={{ padding: "0 150px" }}
+      >
+        <div className="logo flex items-center gap-[10px] cursor-pointer">
+          <BrainCircuit size={26} color="#7e22ce" />
+          <span className="text-white font-bold text-2xl ml-2">CodeGem</span>
+        </div>
+        <div className="icons flex items-center gap-[20px]">
+          <i className="cursor-pointer transition-all duration-300 hover:scale-125 hover:text-yellow-500">
+            <Sun />
+          </i>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
