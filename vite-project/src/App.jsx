81dfa23c0c1420863fa39a78efb9c008c49b274f
Heshani833 <heshanishehana123@gmail.com>
2025-09-23 21:12:53 +0530

    Updated App.jsx

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -2,8 +2,16 @@ import React from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Editor from "@monaco-editor/react";
+import Select from "react-select";
 
 const App = () => {
+  const options = [
+    { value: "chocolate", label: "Chocolate" },
+    { value: "strawberry", label: "Strawberry" },
+    { value: "vanilla", label: "Vanilla" },
+  ];
+
+  const changeLang = () => {};
   return (
     <>
       <Navbar />
@@ -12,7 +20,19 @@ const App = () => {
         style={{ height: "calc(100vh - 90px)" }}
       >
         <div className="left h-[80%] w-[50%]">
-          <Editor height="100%" language="javascript" value="// some comment" />
+          <Select
+            value={options[0]}
+            onChange={(e) => {
+              console.log(e);
+            }}
+            options={options}
+          />
+          <Editor
+            height="100%"
+            theme="vs-dark"
+            language="javascript"
+            value="// some comment"
+          />
         </div>
       </div>
     </>
